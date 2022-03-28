fb0b1640af8df17f350489619a9a3ec510cfa675
Nathaniel Amadi <amadinathaniel@gmail.com>
2022-03-28 19:54:21 +0100

    Added auxillary-projects files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # auxillary-projects
-Shell script to create Linux users with initialized SSH profiles
+
+### Shell script to create Linux users with initialized SSH profiles
+
+### Demo
+[onboarding_users_demo](onboarding_users_demo.gif "onboarding_users_demo")
+
---
+++ onboarding_users.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Onboarding user script.
+#
+# Copyright (c) 2022 Nathaniel Amadi
+#
+# This script will check if group exists and create one if it does not
+# This script will read a CSV file that contains 20 new Linux users.
+# This script will create each user on the server and add to an existing group called 'Developers'.
+# This script will first check for the existence of the user on the system, before it will attempt to create that it.
+# The user that is being created also must also have a default home folder
+# Each user should have a .ssh folder within its HOME folder. If it does not exist, then it will be created.
+# For each user’s SSH configuration, We will create an authorized_keys file and add the below public key.
+
+userfile=$(cat names.csv)
+PASSWORD=password
+groupname=developers
+
+# To ensure the user running this script has sudo privilege
+if [ "$(id -u)" -eq 0 ]; then
+
+# Check if group exists and create one if it does not
+    grep -i '^developers' /etc/group;
+	if  [ $? -eq 0 ]; then
+		echo -e "The group $groupname exists..\n"
+	else
+		sudo groupadd $groupname
+		echo -e "The group $groupname has been created..\n"
+	fi
+
+# Reading the CSV file
+	for user in $userfile;
+	do
+        echo $user
+        if id "$user" &>/dev/null
+        then
+            echo "User Exist"
+        else
+
+# This will create a new user
+        useradd -m -d /home/$user -s /bin/bash -G $groupname  $user
+        echo "New User Created"
+        echo
+
+
+# This will create a ssh folder in the user home folder
+        su - -c "mkdir ~/.ssh" $user
+        echo ".ssh directory created for new user"
+        echo
+
+# We need to set the user permission for the ssh dir
+         su - -c "chmod 700 ~/.ssh" $user
+         echo "user permission for .ssh directory set"
+         echo
+
+# This will create an authorized-key file
+        su - -c "touch ~/.ssh/authorized_keys" $user
+        echo "Authorized Key File Created"
+        echo
+
+# We need to set permission for the key file
+        su - -c "chmod 600 ~/.ssh/authorized_keys" $user
+        echo "user permission for the Authorized Key File set"
+        echo
+
+# We need to create and set public key for users in the server
+        cp -R "/home/ubuntu/Shell/id_rsa.pub" "/home/$user/.ssh/authorized_keys"
+        echo "Copyied the Public Key to New User Account on the server"
+        echo
+        echo
+
+        echo "USER CREATED"
+
+# Generate a password.
+sudo echo -e "$PASSWORD\n$PASSWORD" | sudo passwd "$user" 
+sudo passwd -x 5 $user
+            fi
+        done
+    else
+    echo "Only Admin Can Onboard A User"
+fi
+	
